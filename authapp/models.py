2c7e50ba8ca6e3d0a9c2ec26ef63ac4ca3459da5
Tasin44 <tasinmahmud44@gmail.com>
2026-07-01 15:20:12 +0600

    custom user manager class created

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
+# ─────────────────────────────────────────────
+# CUSTOM USER MANAGER
+# ─────────────────────────────────────────────
+class TeacherManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required.")
+        email = self.normalize_email(email).lower()
+        teacher = self.model(email=email, **extra_fields)
+        teacher.set_password(password)
+        teacher.save(using=self._db)
+        return teacher
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
+        return self.create_user(email, password, **extra_fields)
