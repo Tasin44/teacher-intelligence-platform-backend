c2342252894565c4274b8179a2de16a569ef7808
Tasin44 <tasinmahmud44@gmail.com>
2026-07-03 15:05:11 +0600

    urls.py of groupapp created and defaultrouter defined

---
+++ groupapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import GenerateGroupsView,GroupViewSet,GroupStatsView,GroupGenerationHistoryView
+
+
+router=DefaultRouter()
+
+
+
+
+
+
+
+
+
+
+
+
+
