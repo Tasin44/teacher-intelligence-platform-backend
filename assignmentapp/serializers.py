9800b59b91ac2cd5e0b1f35f2c59485a128f9c3b
Tasin44 <tasinmahmud44@gmail.com>
2026-07-06 10:23:43 +0600

    AssignmentCreateSerializer meta class defined

--- assignmentapp/serializers.py
+++ assignmentapp/serializers.py
@@ -8,7 +8,10 @@ class AssignmentCreateSerializer(serializers.ModelSerializer):
     target_student_roll = serializers.CharField(write_only=True, required=False, allow_blank=True)
     target_group_id = serializers.IntegerField(write_only=True, required=False, allow_null=True)
 
-
+    class Meta:
+        model = Assignment
+        fields = ["title", "subject", "target_type", "target_student_roll", "target_group_id",
+                  "ai_difficulty", "ccss_code", "due_date", "instructions", "number_of_questions"]
 
 
 
