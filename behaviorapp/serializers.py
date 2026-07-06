7e3763be5a8d9d03faf2571689fb324a1311676e
Tasin44 <tasinmahmud44@gmail.com>
2026-07-06 12:52:54 +0600

    validate_engagement_rating and event_date created

--- behaviorapp/serializers.py
+++ behaviorapp/serializers.py
@@ -8,5 +8,14 @@ class BehaviorFeedbackSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BehaviorFeedback
-        fields = ["behavior_id", "student_roll", "event_date", "incident_classification",
-                  "engagement_rating", "observation_note"]
+        fields = ["behavior_id", "student_roll", "event_date", "incident_classification","engagement_rating", "observation_note"]
+
+    def validate_engagement_rating(self, value):
+        if not (1 <= value <= 5):
+            raise serializers.ValidationError("engagement_rating must be between 1 and 5")
+        return value
+
+    def validate_event_date(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("event_date cannot be in the future")
+        return value       
