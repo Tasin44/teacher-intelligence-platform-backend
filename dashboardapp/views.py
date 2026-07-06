e942cfac3165b97bc4e8277ec9a24fd04e3d93b4
Tasin44 <tasinmahmud44@gmail.com>
2026-07-06 11:38:20 +0600

    dashboardapp views.py file with the imports created

--- dashboardapp/views.py
+++ dashboardapp/views.py
@@ -1,3 +1,19 @@
 
 
 
+from django.core.cache import cache
+from django.db.models import Avg, Count
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from coreapp.cache_utils import CACHE_TTL_DASHBOARD, scoped_cache_key
+from coreapp.response import StandardResponseMixin
+from studentapp.models import Student
+from feedbackapp.models import AssignmentFeedback
+from .models import ActivityLog
+
+
+
+
+
+
