87a145df9dd9a978efec821158fe9e93d0cbc7b0
Tasin44 <tasinmahmud44@gmail.com>
2026-07-06 12:18:57 +0600

    validate off date method created

--- attendenceapp/serializers.py
+++ attendenceapp/serializers.py
@@ -6,3 +6,9 @@ class OffDaySerializer(serializers.ModelSerializer):
     class Meta:
         model = OffDay
         fields = ["off_day_id", "off_date"]
+
+    def validate_off_date(self, value):
+        teacher = self.context["request"].user
+        if OffDay.objects.filter(teacher=teacher, off_date=value).exists():
+            raise serializers.ValidationError("This date is already marked as an off day")
+        return value
