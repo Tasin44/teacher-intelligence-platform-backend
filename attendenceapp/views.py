cef2132b6457ad86831eb95be5df6f90317322df
Tasin44 <tasinmahmud44@gmail.com>
2026-07-06 12:34:36 +0600

    views.py of attendenceapp created

--- attendenceapp/views.py
+++ attendenceapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from calendar import monthrange
+from datetime import date
+
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+from coreapp.cache_utils import bump_teacher_cache_version
+from coreapp.response import StandardResponseMixin
+from .models import Attendance, OffDay
+from .serializers import AttendanceSerializer, OffDaySerializer
+
+
+
+
+
