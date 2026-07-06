4fb5d244484c1cb6bdc5d34debbabc38e187257e
Tasin44 <tasinmahmud44@gmail.com>
2026-07-06 10:24:18 +0600

    validate question number & due date method created

--- assignmentapp/serializers.py
+++ assignmentapp/serializers.py
@@ -10,11 +10,18 @@ class AssignmentCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Assignment
-        fields = ["title", "subject", "target_type", "target_student_roll", "target_group_id",
-                  "ai_difficulty", "ccss_code", "due_date", "instructions", "number_of_questions"]
+        fields = ["title", "subject", "target_type", "target_student_roll", "target_group_id","ai_difficulty", "ccss_code", "due_date", "instructions", "number_of_questions"]
 
 
+    def validate_number_of_questions(self, value):
+        if not (1 <= value <= 50):
+            raise serializers.ValidationError("number_of_questions must be between 1 and 50")
+        return value
 
+    def validate_due_date(self, value):
+        if value and value < date.today():
+            raise serializers.ValidationError("due_date cannot be in the past")
+        return value
 
 
 
