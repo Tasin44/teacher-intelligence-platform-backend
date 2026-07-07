1dfdb68f9105a0fae989377ef6ba52bfa3ad685a
Tasin44 <tasinmahmud44@gmail.com>
2026-07-07 09:25:24 +0600

    observationsapp views.py file with all the imports

--- observationsapp/views.py
+++ observationsapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from rest_framework import status,viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from coreapp.permissions import IsOwnerTeacher
+from coreapp.response import StandardResponseMixin
+from .models import Observation
+from .serializers import ObservationSerializer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
