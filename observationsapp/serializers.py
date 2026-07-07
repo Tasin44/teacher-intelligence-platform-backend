ebde4dc80b491078d6cb47f63dde60c5606f9454
Tasin44 <tasinmahmud44@gmail.com>
2026-07-07 09:19:20 +0600

    validate observation date method created

--- observationsapp/serializers.py
+++ observationsapp/serializers.py
@@ -9,3 +9,9 @@ class ObservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Observation
         fields = ["observation_id", "student_roll", "observation_date", "setting_tag", "notes"]
+
+
+    def validate_observation_date(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("observation_date cannot be in the future")
+        return value
