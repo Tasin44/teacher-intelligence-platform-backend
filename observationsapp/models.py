9d601c18881bf12353a142c0d38427b7a0427c2e
Tasin44 <tasinmahmud44@gmail.com>
2026-07-07 09:16:07 +0600

    models.py of observationapp created

--- observationsapp/models.py
+++ observationsapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Observation(models.Model):
+    class SettingTag(models.TextChoices):
+        SMALL_GROUP = "small_group", "Small Group"
+        ONE_TO_ONE = "one_to_one", "1:1"
+        WHOLE_CLASS = "whole_class", "Whole Class"
+
+    observation_id = models.BigAutoField(primary_key=True)
+    student = models.ForeignKey("students.Student", on_delete=models.CASCADE,related_name="observations", db_index=True)
+    observation_date = models.DateField()
+    setting_tag = models.CharField(max_length=15, choices=SettingTag.choices)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "observations"
+        indexes = [models.Index(fields=["student", "observation_date"])]
+        ordering = ["-observation_date"]
